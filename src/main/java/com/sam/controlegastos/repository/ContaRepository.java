b42d0166f17b3a38611aba100769edbeb5f5d4e2
Samuel Victor <samuel2santos5@gmail.com>
2025-09-11 22:28:28 -0300

    feat (repository): adiciona método findBydataBetween no repository

--- src/main/java/com/sam/controlegastos/repository/ContaRepository.java
+++ src/main/java/com/sam/controlegastos/repository/ContaRepository.java
@@ -3,6 +3,11 @@ package com.sam.controlegastos.repository;
 import com.sam.controlegastos.model.Conta;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDate;
+import java.util.List;
+
 
 public interface ContaRepository extends JpaRepository <Conta, Long> {
+    //Busca contas, por intervalo
+    List<Conta> findByDataBetween(LocalDate inicio, LocalDate fim);
 }
