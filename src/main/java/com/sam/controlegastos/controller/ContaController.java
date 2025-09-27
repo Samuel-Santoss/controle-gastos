c762bfd4cb01f3f38b14c171864180ed7702116e
Samuel Victor <samuel2santos5@gmail.com>
2025-09-27 19:10:16 -0300

    feat: implementa endpoint POST /contas com validação e ajustes no repository

---
+++ src/main/java/com/sam/controlegastos/controller/ContaController.java
@@ -0,0 +1,38 @@
+package com.sam.controlegastos.controller;
+
+import com.sam.controlegastos.dto.ContaCreateDTO;
+import com.sam.controlegastos.dto.ContaResponseDTO;
+import com.sam.controlegastos.model.Conta;
+import com.sam.controlegastos.service.ContaService;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/contas")
+public class ContaController {
+
+    private final ContaService contaService;
+
+    public ContaController(ContaService contaService) {
+        this.contaService = contaService;
+    }
+
+    @PostMapping
+    public ResponseEntity<ContaResponseDTO> inserirConta(@Valid @RequestBody ContaCreateDTO dto){
+        Conta conta = new Conta();
+        conta.setDescricao(dto.getDescricao());
+        conta.setValor(dto.getValor());
+        conta.setDataCadastro(dto.getDataCadastro());
+
+        //Inserir conta no banco
+        Conta contaSalva = contaService.inserirConta(conta);
+
+        //Converte em DTO
+        ContaResponseDTO responseDTO = new ContaResponseDTO(contaSalva);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(responseDTO);
+    }
+
+}
--- src/main/java/com/sam/controlegastos/repository/ContaRepository.java
+++ src/main/java/com/sam/controlegastos/repository/ContaRepository.java
@@ -2,12 +2,13 @@ package com.sam.controlegastos.repository;
 
 import com.sam.controlegastos.model.Conta;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
 import java.util.List;
 
-
+@Repository
 public interface ContaRepository extends JpaRepository <Conta, Long> {
     //Busca contas, por intervalo
-    List<Conta> findByDataBetween(LocalDate inicio, LocalDate fim);
+    List<Conta> findByDataCadastroBetween(LocalDate inicio, LocalDate fim);
 }
